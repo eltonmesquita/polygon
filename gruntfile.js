6d44d390a985ce013e0575e3dbf86e5a6f66e4aa
Elton Mesquita <elton@intersect.com.br>
2014-06-27 18:58:53 -0300

    Major refectoring, updated modernizr, preparing to version .3

--- gruntfile.js
+++ gruntfile.js
@@ -1,31 +1,110 @@
-module.exports = function (grunt) {
+module.exports = function(grunt) {
+
   grunt.initConfig({
+
+    // Optmizes the images
+    imagemin: {
+      dynamic: {
+        options: {
+          optimizationLevel: 5,
+        },
+        files: [{
+          expand: true,
+          cwd: 'src/img/',
+          src: ['src/img/**/*.{png,jpg,gif,svg}'],
+          dest: 'build/img/'
+        }]
+      }
+    },
+
+    // Compiles SCSS files
     sass: {
-      files: {
-        expand: true, // allow dynamic building
-        cwd: 'css/', // source files mask
-        src: '**/*.scss',
-        dest: 'css/min/', // destination folder
-        flatten: true, // remove all unnecessary nesting
-        ext: '.css'
+      dist: {
+        options: {
+          sourcemap: 'false',
+          style: 'compressed'
+        },
+        files: {
+          'build/css/style.css': 'src/css/style.scss',
+          'build/css/slick.css': 'src/css/vendor/slick.scss'
+        }
       }
     },
+
+    // Adds all the prefixes in the css files
     autoprefixer: {
-      files: {
-        src: 'css/min/style.css',
-        dest: 'css/min/autoprefixer.css'
+      options: {
+        browsers: ['last 3 versions', 'ie 9', 'ie 8', 'ff 20', 'android 3'],
+        sourcemap: 'false'
+      },
+      no_dest: {
+        // File to output
+        src: 'build/css/style.css',
+        dest: 'build/css/style.css'
+      },
+    },
+
+    // Rem fallback
+    remfallback: {
+      options: {
+        log: true,
+        replace: false,
+      },
+      your_target: {
+        files: {
+          'css/style.css': ['css/style.css']
+        },
+      },
+    },
+
+    // Minifies and merges the javascripts
+    uglify: {
+      options: {
+        mangle: false
+      },
+      my_target: {
+        files: {
+          'build/js/main.js': ['src/js/main.js'],
+          'build/js/modernizr.js': ['src/js/modernizr.js'],
+        }
       }
     },
-    cssmin: {
-      minify: {
-        src: 'css/min/autoprefixer.css',
-        dest: 'css/min/min.css'
+
+    // The watch task + livereload :D
+    watch: {
+      options: {
+        livereload: true,
+      },
+      php: {
+        files: ['**/**.php']
+      },
+      css: {
+        files: ['src/css/**/**.scss'],
+        tasks: ['sass', 'autoprefixer']
+      },
+      uglify: {
+        files: ['src/js/**/**.js'],
+        tasks: ['uglify']
+      },
+      imagemin: {
+        files: ['src/img/**/*.{png,jpg,gif}'],
+        tasks: ['imagemin']
       }
     }
+
   });
-  // load plugins
+
+  // loads plugins
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-autoprefixer');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.registerTask('default', ['sass', 'autoprefixer', 'cssmin']);
-};
+  grunt.loadNpmTasks('grunt-remfallback');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // register tasks
+  grunt.registerTask('default', ['watch']);
+  grunt.registerTask('build', ['sass', 'imagemin', 'autoprefixer', 'remfallback', 'uglify']);
+
+
+}
--- watch.bat
+++
@@ -1,6 +0,0 @@
-:: Outputs human readable CSS - DEVELOPMENT
-sass --watch css/style.scss:css/style.css --style expanded
-
-:: Uncomment to output compressed CSS - DEPLOYMENT
-#sass --watch css/style.scss:css/style.min.css --style compressed
-

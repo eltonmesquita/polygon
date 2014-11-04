c1e4db16950ca1781e947634e73ab078261f1821
Elton Mesquita <elton@intersect.com.br>
2014-11-04 12:29:11 -0300

    Code reformated and moved to a more sane architeture

--- gruntfile.js
+++ gruntfile.js
@@ -2,49 +2,46 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
 
-    // Optmizes the images
+    // Optimizes the images
     imagemin: {
       dynamic: {
         options: {
-          optimizationLevel: 5,
+          optimizationLevel: 6,
         },
         files: [{
           expand: true,
-          cwd: 'src/img/',
-          src: ['src/img/**/*.{png,jpg,gif,svg}'],
+          cwd: 'src/img',
+          src: ['**/*.{png,jpg,gif}'],
           dest: 'build/img/'
         }]
       }
     },
 
-    // Compiles SCSS files
+    // Compiles SASS files with sourcemaps
     sass: {
       dist: {
         options: {
-          sourcemap: 'false',
-          style: 'compressed'
+          sourcemap: true,
+          style: 'compact'
         },
         files: {
-          'build/css/style.css': 'src/css/style.scss',
-          'build/css/slick.css': 'src/css/vendor/slick.scss'
+          'build/css/style.css': 'src/css/style.scss'
         }
       }
     },
 
-    // Adds all the prefixes in the css files
+    // Auto prefixer, configure it as you need
     autoprefixer: {
       options: {
         browsers: ['last 3 versions', 'ie 9', 'ie 8', 'ff 20', 'android 3'],
-        sourcemap: 'false'
+        map: true
       },
       no_dest: {
-        // File to output
-        src: 'build/css/style.css',
-        dest: 'build/css/style.css'
+        src: 'build/css/*.css'
       },
     },
 
-    // Rem fallback
+    // Fallback for CSS' REM unit
     remfallback: {
       options: {
         log: true,
@@ -52,31 +49,103 @@ module.exports = function(grunt) {
       },
       your_target: {
         files: {
-          'css/style.css': ['css/style.css']
+          'css/style.css': ['css/style.css'],
+          'css/about.css': ['css/about.css'],
+          'css/roundabout.css': ['css/roundabout.css'],
+          'css/stores.css': ['css/stores.css']
         },
       },
     },
 
-    // Minifies and merges the javascripts
+    // Minify your javascripts
     uglify: {
       options: {
         mangle: false
       },
-      my_target: {
+       my_target: {
+        files: [{
+            expand: true,
+            cwd: 'src/js',
+            src: '**/*.js',
+            dest: 'js'
+        }]
+      }
+    },
+
+    // Creates a dedicated ie.css with all the MQ's
+    match_media: {
+      ie: {
         files: {
-          'build/js/main.js': ['src/js/main.js'],
-          'build/js/modernizr.js': ['src/js/modernizr.js'],
+          'build/css/ie.css': ['build/css/style.css']
         }
       }
     },
 
-    // The watch task + livereload :D
+    // Compress CSS
+    csso: {
+      compress: {
+        options: {
+          report: 'gzip',
+          restructure: false
+        },
+        files: {
+          'build/css/**/*.css': ['build/css/**/*.css']
+        }
+      }
+    },
+
+    // Analyses the performance of the page(s)
+    devperf: {
+      options: {
+        urls: [
+          'http://localhost:8050'
+        ]
+      }
+    },
+
+    // Takes screenshots of the page(s) in several resolutions
+    pageres: {
+        index: {
+            options: {
+                url: 'http://localhost:8050',
+                sizes: [
+                          '1920x1080',
+                          '1440x1024',
+                          '1280x768',
+                          '1024x768',
+                          '1024x600',
+                          '800x600',
+                          '604x966',
+                          '375x667',
+                          '320x480',
+                          '240x380'
+                        ],
+                dest: 'screenshots/index',
+                delay: 2
+            }
+        }
+    },
+
+    // Launchs a server with livereload included
+    connect: {
+      client: {
+        options: {
+          port: 8050,
+          base: 'build/',
+          hostname: 'localhost',
+          livereload: true
+        }
+      }
+    },
+
+    // Configures the watch task
     watch: {
       options: {
         livereload: true,
       },
-      php: {
-        files: ['**/**.php']
+      html: {
+        files: ['**/**.html'],
+        tasks: ['special-html']
       },
       css: {
         files: ['src/css/**/**.scss'],
@@ -94,17 +163,22 @@ module.exports = function(grunt) {
 
   });
 
-  // loads plugins
+  // Load the plugins
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-autoprefixer');
   grunt.loadNpmTasks('grunt-remfallback');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-match-media');
+  grunt.loadNpmTasks('grunt-csso');
+  grunt.loadNpmTasks('grunt-devperf');
+  grunt.loadNpmTasks('grunt-pageres');
 
-  // register tasks
+  // Register the tasks
   grunt.registerTask('default', ['watch']);
-  grunt.registerTask('build', ['sass', 'imagemin', 'autoprefixer', 'remfallback', 'uglify']);
-
+  grunt.registerTask('perf',    ['devperf', 'pageres']);
+  grunt.registerTask('build',   ['sass', 'imagemin', 'autoprefixer', 'match_media', 'remfallback', 'csso', 'uglify']);
 
 }
--- readme.md
+++ readme.md
@@ -1,4 +1,23 @@
-WIP
-Polygon Framework by Elton Mesquita
+# Polygon.css
+### by Elton Mesquita
 
-A flexible SASS based (mini)framework built with small and medium projects in mind. It doesn't provide style, but just the bare minimal boilerplate to build modern and well coded pages.
+A CSS boilerplate in SASS for the today's and tomorrow's web.
+
+## What?
+
+A simple CSS boilerplate in SASS (SCSS syntax) and a Gruntfile to make it easy to start non-bloated web projects.
+
+You get [normalize.css](http://necolas.github.io/normalize.css/), a grid, some base CSS and a Gruntfile that brings lots of what any front-end developer should need, pre-configured.
+
+## Why?
+
+There are lots of frameworks out there, some are minimal and some are full sets with UI and styling included, and most of them are great and can bring great value but none of them was what I needed. They usually are bloated, with rigid structure and have a big learning curve. All I needed was normalize.css, a small set of Styles pre-defined and a grid. Well, here we are ;D
+
+To the day I have used it in every web page/app I do and the results are great. The CSS and HTML complexity has lowered a lot, compared to the previous Frameworks I used, and the final CSS file is around 30kb heavy in average, even in bigger web sites :D.
+
+## How?
+
+It's pretty straight forward. Change the variables as you need, choose your classes names and just write your CSS. You don't need to use grunt, but it's a much more efficient and complete way to use Polygon.css as it will compile the SCSS files, add the prefixes, add the REM fallback and minify your CSS. Without it you'll have to deal with everything by your own.
+
+## Compatibility
+IE8+, FireFox 2+, Chrome 10+, Safari 5+, iOS' Safari 5.1+, Android 2+, Opera 9.6+, Opera Mini 5+, Opera Mobile 10+, Blackberry 10+.
